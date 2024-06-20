1a9950bea9875ab749886b878d9f5c5f904e94dd
Junior Alcantara <jrlevita09@hotmail.com>
2024-06-20 10:54:34 -0300

    Criando Projeto e interface do Reprodutor musical

---
+++ .history/src/Ipod_20240620105433.java
@@ -0,0 +1,21 @@
+public class Ipod implements reprodutorMusica {
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+        
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música");
+        
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+        
+    }
+
+}
---
+++ src/reprodutorMusica.java
@@ -0,0 +1,5 @@
+public interface reprodutorMusica {
+    public void selecionarMusica();
+    public void pausar();
+    public void tocar();
+}
